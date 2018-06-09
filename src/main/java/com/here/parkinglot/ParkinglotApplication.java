051a556bc0a901d1f78d79a50c9703fffeb00d14
sravya <sravya.patnaikuni@gmail.com>
2018-06-09 18:59:49 +0530

    initial skeleton

---
+++ src/main/java/com/here/parkinglot/ParkinglotApplication.java
@@ -0,0 +1,12 @@
+package com.here.parkinglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkinglotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkinglotApplication.class, args);
+	}
+}
